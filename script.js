477e60beb118a3747ad2873fbbdf3ca57223600a
YuriMendess <contato.yurimendess@gmail.com>
2021-11-06 17:10:25 -0300

    um ponto e vírgula que estava faltando :)

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function createCactus() {
             background.removeChild(cactus);
         } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) {
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo!</h1>'
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo!</h1>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
